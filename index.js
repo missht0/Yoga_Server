5d0f75da2b2b58db99dd8feeab15c8ddd435188e
missht0 <missht0@163.com>
2022-09-05 10:18:42 +0800

    gzip压缩

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const path = require('path');
+const cors = require('cors');
 // 导入express
 const express = require('express');
+const compression = require('compression');
+
 // 创建express实例
 const app = express();
 // 导入路由模块
 const router = require('./router');
 // 注册路由模块
+app.use(compression());
 app.use(router);
-
+app.use(cors());  //允许跨域
 // 调用监听函数
 app.listen(8000, () => {
     console.log('server is running at port 8000');
